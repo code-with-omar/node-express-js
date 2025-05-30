ae7b15815a4db6637c796fa323757b044a9a1173
code-with-omar <omarfaruk65142@gmail.com>
2025-05-30 17:53:50 +0600

    readfile

--- node-js/fs-module/index.js
+++ node-js/fs-module/index.js
@@ -1,16 +1,25 @@
 const fs = require("fs");
 
-fs.writeFile("demo.txt", "This is sample text", (err) => {
+// fs.writeFile("demo.txt", "This is sample text", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
+// fs.appendFile("demo.txt", "\nMy name is Md. Omar Faruk", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
+
+// readFile
+fs.readFile("demo.txt", "utf-8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Successful");
-  }
-});
-fs.appendFile("demo.txt", "\nMy name is Md. Omar Faruk", (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successful");
+    console.log(data);
   }
 });
