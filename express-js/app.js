a5d5f8c36efe7817731b1cc49797ae1525a241fb
code-with-omar <omarfaruk65142@gmail.com>
2025-06-01 16:36:58 +0600

    static

--- express-js/app.js
+++ express-js/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/user.routers");
+
+app.use(express.static("public"));
 // http request
 app.use("/user", userRouter);
 
@@ -10,12 +12,11 @@ app.use((req, res, next) => {
 });
 app.get("/", (req, res) => {
   res.statusCode = 200;
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "./public/index.html");
 });
 app.use((req, res) => {
   res.send("<h1>404 !! page not found</h1>");
 });
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
